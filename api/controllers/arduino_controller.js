35ce3f8ec168eaf096ab0871c8430ee86948a041
Shaiane Boesing Rodrigues Borges <shaianeboesingrb@gmail.com>
2023-05-07 11:26:46 -0300

    teste arduino intensidade

--- api/controllers/arduino_controller.js
+++ api/controllers/arduino_controller.js
@@ -1,4 +1,44 @@
+const net = require('net');
+
 exports.index = function(req, res) {
-    res.send("teste");
+    res.send("Hello world");
 };
 
+exports.changeIntensity = function(req, res) {
+    const arduinoIP = '192.168.3.193';
+    const arduinoPort = 80;
+  
+    const intensity = req.params.intensity;
+  
+    if (intensity >= 0 && intensity <= 255) {
+      const mensagem = `${intensity}\n`;
+  
+      arduinoConnect(arduinoIP, arduinoPort, mensagem);
+  
+      res.send(`Mensagem enviada para alterar a intensidade do LED para ${intensity}.`);
+    } else {
+      res.status(400).send('A intensidade deve ser um valor entre 0 e 255.');
+    }
+  };
+
+function arduinoConnect(arduinoIP, arduinoPort, mensagem) {
+    const arduinoSocket = net.connect({ host: arduinoIP, port: arduinoPort }, () => {
+      console.log('Conexão estabelecida com o Arduino!');
+    });
+  
+    arduinoSocket.on('connect', () => {
+      arduinoSocket.write(mensagem);
+  
+      arduinoSocket.end();
+    });
+  
+    arduinoSocket.on('data', (data) => {
+      console.log(`Mensagem recebida do Arduino: ${data}`);
+    });
+  
+    arduinoSocket.on('error', (error) => {
+      console.error(`Erro ao conectar com o Arduino: ${error}`);
+    });
+  }
+  
+  
--- api/routes.js
+++ api/routes.js
@@ -3,5 +3,6 @@ const arduino = require('./controllers/arduino_controller');
 
 // Login
 routes.get('/', arduino.index);
+routes.get('/led/:intensity', arduino.changeIntensity);
 
 module.exports = routes;
